c0c016626836dda01f78172d5c86f18bd0c63d7e
AlexLeXX444 <alexlexx444@yandex.ru>
2022-05-24 22:32:02 +0500

    решил задачу 8

--- lesson1_task8/program.py
+++ lesson1_task8/program.py
@@ -1,7 +1,33 @@
 # Напишите программу, которая проверит истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
-# (!(x_input && y_input) == !x_input || !y_input)
+counter = 0
+
+def is_true (x,y):
+    if (not(x or y)) == (not(x) and not(y)):
+        return True
+    else:
+        return False
+
+x = True
+y = True
+print(f'При Х = 1 и Y = 1 функция возвращает значение {is_true(x,y)}.')
+if is_true(x,y): counter += 1
 
 x = True
 y = False
+print(f'При Х = 1 и Y = 0 функция возвращает значение {is_true(x,y)}.')
+if is_true(x,y): counter += 1
 
-print(x)
+x = False
+y = True
+print(f'При Х = 0 и Y = 1 функция возвращает значение {is_true(x,y)}.')
+if is_true(x,y): counter += 1
+
+x = False
+y = False
+print(f'При Х = 0 и Y = 0 функция возвращает значение {is_true(x,y)}.')
+if is_true(x,y): counter += 1
+
+print()
+
+if counter == 4: print('Функция истинна!')
+else: print('Функция ложна!')
